2552861b5b68225f530cfcc59ebab4ce5e415867
Aaron <aaronjeffersonvillanueva15@outlook.com>
2020-09-24 22:45:03 +0800

    User model, customer API

---
+++ WooCommerceAPI/Models/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WooCommerceAPI.Models
+{
+    public class Billing
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string company { get; set; }
+        public string address_1 { get; set; }
+        public string address_2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; }
+        public string country { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+
+    public class Shipping
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string company { get; set; }
+        public string address_1 { get; set; }
+        public string address_2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+    }
+
+    public class Collection
+    {
+        public string href { get; set; }
+    }
+
+    public class Links
+    {
+        public IList<Self> self { get; set; }
+        public IList<Collection> collection { get; set; }
+    }
+
+    public class User
+    {
+        public int id { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_created_gmt { get; set; }
+        public DateTime date_modified { get; set; }
+        public DateTime date_modified_gmt { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string role { get; set; }
+        public string username { get; set; }
+        public Billing billing { get; set; }
+        public Shipping shipping { get; set; }
+        public bool is_paying_customer { get; set; }
+        public string avatar_url { get; set; }
+        public IList<object> meta_data { get; set; }
+        public Links _links { get; set; }
+    }
+}
